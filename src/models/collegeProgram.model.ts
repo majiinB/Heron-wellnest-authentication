7b43c2f9750594e4ceba13eaef40e27661510522
Arthur Artugue <artuguearthur@gmail.com>
2025-10-15 22:19:53 +0800

    Added college program model

---
+++ src/models/collegeProgram.model.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, Index, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CollegeDepartment } from "./collegeDepartment.model.js";
+
+
+@Entity("college_programs")
+export class CollegeProgram {
+  @PrimaryGeneratedColumn("uuid")
+  program_id!: string
+
+  @Column({ type: "varchar", length: 255 , nullable: false})
+  program_name!: string;
+
+  @OneToOne(()=> CollegeDepartment, {onDelete: "CASCADE"})
+  @JoinColumn({name: "department_id"})
+  college_department!: CollegeDepartment
+
+  @Column({type: "boolean", default: false})
+  is_deleted!: boolean
+
+  @CreateDateColumn({type: "timestamptz"})
+  created_at!: Date;
+
+  @UpdateDateColumn({type: "timestamptz"})
+  updated_at!: Date;
+
+}
